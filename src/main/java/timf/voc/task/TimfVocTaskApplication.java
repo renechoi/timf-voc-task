05d1a0e1d41f8e3db34514b2fe6b28ba55941b1b
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-23 20:57:33 +0900

    feat: initial project set up

---
+++ src/main/java/timf/voc/task/TimfVocTaskApplication.java
@@ -0,0 +1,13 @@
+package timf.voc.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimfVocTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimfVocTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/timf/voc/task/TimfVocTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package timf.voc.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimfVocTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
