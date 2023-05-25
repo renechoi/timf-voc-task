323cd88793c552ca2f66d30e43e1474ca6a01bec
renechoi <115696395+renechoi@users.noreply.github.com>
2023-05-26 00:43:29 +0900

    feat: driver 조회 및 리턴 로직 구현

--- src/main/java/timf/voc/task/service/TransportCompanyService.java
+++ src/main/java/timf/voc/task/service/TransportCompanyService.java
@@ -1,9 +1,15 @@
 package timf.voc.task.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import timf.voc.task.dto.DeliveryDriverDto;
+import timf.voc.task.dto.response.DeliveryDriverMyPageResponse;
+import timf.voc.task.dto.response.VocResponse;
 import timf.voc.task.entity.DeliveryDriver;
 import timf.voc.task.exception.DeliveryDriverNotFoundException;
 import timf.voc.task.repository.DeliveryDriverRepository;
@@ -17,7 +23,24 @@ public class TransportCompanyService {
 	private final TransportCompanyRepository transportCompanyRepository;
 	private final DeliveryDriverRepository deliveryDriverRepository;
 
-	public DeliveryDriver searchDeliveryDriverEntity(Long deliveryDriverId) {
-		return deliveryDriverRepository.findById(deliveryDriverId).orElseThrow(DeliveryDriverNotFoundException::new);
+	public DeliveryDriver searchDeliveryDriverEntity(Long id) {
+		return findById(id);
+	}
+
+	public DeliveryDriverMyPageResponse getMyPage(Long id) {
+		DeliveryDriver deliveryDriver = findById(id);
+
+		return DeliveryDriverMyPageResponse.builder()
+			.deliveryDriver(DeliveryDriverDto.from(deliveryDriver))
+			.vcos(searchDriverVocs(deliveryDriver))
+			.build();
+	}
+
+	private List<VocResponse> searchDriverVocs(DeliveryDriver deliveryDriver) {
+		return deliveryDriver.getVocList().stream().map(VocResponse::from).collect(Collectors.toList());
+	}
+
+	private DeliveryDriver findById(Long id){
+		return deliveryDriverRepository.findById(id).orElseThrow(DeliveryDriverNotFoundException::new);
 	}
 }
